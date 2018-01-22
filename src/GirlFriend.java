25692b2b5daed7925c3ed1f87901934e5189aa1c
WangSiwang <wangwangh01@qq.com>
2018-01-22 20:55:44 +0800

    Initial commit

---
+++ src/GirlFriend.java
@@ -0,0 +1,37 @@
+public class GirlFriend {
+    private String 身高;
+    private String 体重;
+    private String 皂片;
+
+    public static void main(String[] args) {
+        GirlFriend 秋女 = new GirlFriend();
+        秋女.体重 = "怎么会让你们知道";
+        秋女.身高 = "怎么会让你们知道 ";
+        秋女.皂片 ="" +
+                " ,,,,,,,,,,..--!!>>>>;;......,,,,,,,,,,,, \n" +
+                " ,,,,,,..,,CCNNNNNNNNQQ$$!!,,..,,,,,,,,,, \n" +
+                " ,,,,..,,CCNNNNNNQQ$$HHNNQQCC::..,,,,,,,, \n" +
+                " ,,,,,,CCNNOONNNNOOCCCCQQQQOOCC,,..,,,,,, \n" +
+                " ::..>>MMNNNNNNQQHH77??CCOOOOQQ!!..,,,,,, \n" +
+                " ,,::NNMMNNMMNNOOCC??>>>>CCQQNNHH,,,,,,,, \n" +
+                " ,,>>MMMMMMMMNNHHCC>>>>>>??HHNNNN--..,,,, \n" +
+                " ,,HHMMMMMMNNQQ$$77??>>>>>>77NNNN>>..,,,, \n" +
+                " ::NNMMMMMMNNHH77??>>>>>>>>>>QQMM??,,,,,, \n" +
+                " --MMMMMMNNNNOOCC????77CC??!!$$MM??,,,,,, \n" +
+                " ??MMMMMMNNHHCCCC77??CCCC??!!??MM??,,,,:: \n" +
+                " QQMMMMMMNNQQNNCC??>>CCNN$$!!>>MM>>,,:::: \n" +
+                " MMMMMMNN$$7777??>>''>>>>!!;;>>MM'',,:::: \n" +
+                " MMMMMMNN77!!!!??>>''''!!!!--CCMM;;:::::: \n" +
+                " MMMMMMNN77>>>>77??''!!??>>''QQHH,,:::::: \n" +
+                " MMMMMMMMHH777777CC77??????>>NN>>:::::::: \n" +
+                " MMMMMMMMNNCC$$CC????>>CC77$$MM''::;;;;:: \n" +
+                " MMMMMMMMMMHHCC$$$$CC$$$$!!$$CC--::;;OO'' \n" +
+                " MMMMMMMMMMQQHHCCCC$$HHHH''>>!!!!!!CCMMQQ \n" +
+                " MMMMMMMMMMOOOOHH$$$$OOMM$$>>77HHNNMMMMMM \n" +
+                " MMMMMMMMMMOO$$HHHHHHOOMMQQ??NNMMMMMMMMMM \n" +
+                " NNMMMMMMMMNNHH$$$$CC$$MMMMMMMMMMMMMMMMMM \n" +
+                " NNNNMMMMMMNNHH$$CCCC77HHNNMMMMMMMMMMMMMM \n" +
+                " NNMMMMMMMMMM7777CCCC??''QQNNMMMMMMMMMMMM ";
+        ;
+    }
+}
